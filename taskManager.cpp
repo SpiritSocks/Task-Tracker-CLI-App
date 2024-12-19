54532bc882e636dfb4e8c01ebee7600d2ee024b7
Valera <vdteterich@gmail.com>
2024-12-20 00:08:00 +0300

    taskManager.cpp

--- taskManager.cpp
+++ taskManager.cpp
@@ -1,10 +1,10 @@
 #include "header.h"
 #include <iostream>
 
-Task::Task() = default;
-Task::Task(const std::string& name, const std::string& priority, const std::string& dueDate) {
+Task::Task() = default; //Defualt constructor 
+Task::Task(const std::string& name, const std::string& priority, const std::string& dueDate) { //Actual constructor that creates the Task object using the input parameters
     this->name = name;
     this->priority = priority;
     this->dueDate = dueDate;
 }
-Task::~Task(){};
+Task::~Task(){}; //Destructor
